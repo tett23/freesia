e4edf6f88dd75c5aec20a227543bb471f2eb20bf
tett23 <tett23@gmail.com>
2013-04-03 02:36:20 +0900

    データ型の編集画面を作成

--- app/controllers/settings/columns.rb
+++ app/controllers/settings/columns.rb
@@ -17,4 +17,27 @@ Freesia::App.controllers :setting_columns, map: '/settings' do
       render 'settings/columns/new'
     end
   end
+
+  get :edit, with: :id do |id|
+    @data_column = DataColumn.get(id)
+    return error 404 if @data_column.nil?
+    return error 403 unless @data_column.account_id == current_account.id
+
+    render 'settings/columns/edit'
+  end
+
+  put :update, with: :id do |id|
+    @data_column = DataColumn.get(id)
+    return error 404 if @data_column.nil?
+    return error 403 unless @data_column.account_id == current_account.id
+
+    if @data_column.update(params[:data_column])
+      message = "#{@data_column.name}:#{@data_column.type} を編集しました"
+      flash[:success] = message
+      redirect url(:settings, :index)
+    else
+      render 'settings/columns/new'
+    end
+  end
+
 end
