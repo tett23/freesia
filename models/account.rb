14f7b67e62e1096169864a6402b08983a8c61a1c
tett23 <tett23@gmail.com>
2013-04-08 15:23:07 +0900

    アカウント作成時にscreen_nameを記録する

--- models/account.rb
+++ models/account.rb
@@ -5,14 +5,18 @@ class Account
   property :name, String
   property :email, String
   property :role, String
+  property :screen_name, String
   property :uid, String
   property :provider, String
+  property :updated_at, DateTime
+  property :created_at, DateTime
 
   def self.create_with_omniauth(auth)
     account = first_or_create({
       :uid => auth['uid'],
       :provider => auth['provider'],
       :name => auth['info']['name'],
+      :screen_name => auth['info']['nickname'],
       :role => :users
     })
 
