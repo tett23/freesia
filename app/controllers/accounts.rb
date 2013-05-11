e798139145c23c5e4f4c8cdd07a7ed76c570ea9e
tett23 <tett23@gmail.com>
2013-05-11 17:27:53 +0900

    accounts内でpassするとエラーが出る

--- app/controllers/accounts.rb
+++ app/controllers/accounts.rb
@@ -3,12 +3,12 @@
 Freesia::App.controllers :accounts, map: '/' do
   before do
     @page_header = :accounts
-    @account = Account.detail(params[:screen_name])
-    #add_breadcrumbs(@account.name, url(:accounts, :index, screen_name: @account.screen_name))
   end
 
   get :index, map: '/:screen_name', priority: :low do |screen_name|
+    pass if screen_name == 'settings'
     @account = Account.first(screen_name: screen_name.to_sym)
+    add_breadcrumbs(@account.name, url(:accounts, :index, screen_name: @account.screen_name))
 
     render 'accounts/index'
   end
