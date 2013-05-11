4d779fef77322606d37f1e8fd139cafac7ea4d45
tett23 <tett23@gmail.com>
2013-05-11 17:30:29 +0900

    パンくず追加

--- app/controllers/settings.rb
+++ app/controllers/settings.rb
@@ -1,7 +1,10 @@
+# coding: utf-8
+
 Freesia::App.controllers :settings do
   get :index, map: '/settings', priority: :high do
     @data_columns = DataColumn.list(current_account.id)
     @datasets = Dataset.list(current_account.id)
+    add_breadcrumbs('設定', url(:settings, :index))
 
     render 'settings/index'
   end
