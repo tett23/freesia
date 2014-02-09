1c1b5d170239c98ff3dc23994655602bdbdf2d13
tett23 <tett23@gmail.com>
2014-02-10 01:18:55 +0900

    rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freesia::Application.config.secret_key_base = '1cfb9536960ccb729aaf0bf120805e768de1247db6f51b3df3e7ea359493da68ec9ba358dc3501e3ea96bb9f6edbfd594030664a9e09bc28fc9687dd14279bb0'
